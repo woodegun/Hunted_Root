18b6d27df16dbeabd6302eba30749c5aec0de41b
woode <woodegun@gmail.com>
2023-01-31 20:29:58 +0300

    Простой противник и NevMesh

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent _navMeshAgent;
+    private Transform target;
+
+    private void Awake()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
     void Update()
     {
-        
+        if (target != null)
+        {
+            _navMeshAgent.destination = target.position;
+        }
     }
 }
