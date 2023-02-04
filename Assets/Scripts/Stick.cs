0b430bfcde3f831b307a99307fcc372e1d1e3bc1
woode <woodegun@gmail.com>
2023-02-04 18:45:12 +0300

    Звук палки

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -1,16 +1,23 @@
+using System;
 using UnityEngine;
 
 public class Stick : MonoBehaviour
 {
+    private AudioSource _audioSource;
     public delegate void OnBranchCrunches(Transform transform);
 
     public static event OnBranchCrunches onBranchCrunches;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>() != null)
         {
-            Debug.Log("Хруст");
+            _audioSource.Play();
             onBranchCrunches?.Invoke(transform);
         }
     }
